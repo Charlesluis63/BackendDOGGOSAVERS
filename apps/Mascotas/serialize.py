505b5adbac84d20851f1d42de51ea9a79b7eb021
Diego <diegoand2009@hotmail.com>
2020-01-12 21:44:23 -0500

    correccion de errores

---
+++ apps/Mascotas/serialize.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from .models import *
+
+class Mascotaserialize(ModelSerializer):
+    class Meta:
+        model =  Mascota
+        #No veo necesario agregar contraseña
+        fields = ('id','username','estado','tipo_usuario')
+
+class Mascota_adopcion_serialize (ModelSerializer):
+    class Meta:
+        model= Mascota_En_Adopcion
+        fields = ('id','id_mascota','id_user','puntaje_juego')
--- apps/datos_usuarios/views.py
+++ apps/datos_usuarios/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.views import APIView
-from .serializer import Usuarioserialize,Persona_serialize
+from .serializer import Usuarioserialize,Personaserialize
 from .models import Persona,Usuario
+from rest_framework.decorators import api_view # new
+from rest_framework.response import Response # new
+from rest_framework.reverse import reverse # new
 
 @api_view(['GET']) # new
 def api_root(request, format=None):
@@ -16,19 +19,19 @@ def api_root(request, format=None):
 # Create your views here.
 
 class Usuario_List(generics.ListCreateAPIView):
-    queryset = Persona.objects.all()
-    serializer_class = Persona_serialize
+    queryset = Usuario.objects.all()
+    serializer_class = Usuarioserialize
 
 
 class Usuario_Detail(generics.RetrieveUpdateDestroyAPIView):
-    queryset =Persona.objects.all()
-    serializer_class = Persona_serialize
+    queryset =Usuario.objects.all()
+    serializer_class = Usuarioserialize
 
 class Persona_List(generics.ListCreateAPIView):
     queryset = Persona.objects.all()
-    serializer_class = Persona_serialize
+    serializer_class = Personaserialize
 
 
 class Persona_Detail(generics.RetrieveUpdateDestroyAPIView):
     queryset =Persona.objects.all()
-    serializer_class = Persona_serialize
+    serializer_class = Personaserialize
