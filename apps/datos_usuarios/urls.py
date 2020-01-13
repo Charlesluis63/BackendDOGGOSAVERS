6f848990ef7d24870622a922f970b06a464a971d
Diego <diegoand2009@hotmail.com>
2020-01-12 22:13:35 -0500

    errores

--- apps/datos_usuarios/urls.py
+++ apps/datos_usuarios/urls.py
@@ -4,10 +4,10 @@ from apps.datos_usuarios import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('usuario/', views.Usuario_List.as_view()),
-    path('usuario/<int:pk>/', views.Usuario_Detail.as_view()),
-    path('persona/', views.Persona_List.as_view()),
-    path('persona/<int:pk>/', views.Persona_Detail.as_view()),
+    path('usuario/', views.Usuario_List.as_view(), name='usuario-list'),
+    path('usuario/<int:pk>/', views.Usuario_Detail.as_view(), name='usuario-detail'),
+    path('persona/', views.Persona_List.as_view(), name='persona-list'),
+    path('persona/<int:pk>/', views.Persona_Detail.as_view(), name='persona-detail'),
     path('', views.api_root),
 
 ]
